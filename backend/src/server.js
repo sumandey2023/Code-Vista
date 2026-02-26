f78494f98dbb33718e77cfd544d715e4d1d22361
sumandey2023 <sdey99901@gmail.com>
2026-02-26 12:28:49 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+const PORT = ENV.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
